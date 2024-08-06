4e8522017d401920af3a8f94edb051dcde8c5cd3
Gennadii Mareichev <gennadii.mareichev@gennadiis-macbook-pro.home>
2024-08-06 12:16:28 +0200

    changed send button content

--- src/components/chatInput/chatInput.tsx
+++ src/components/chatInput/chatInput.tsx
@@ -2,7 +2,9 @@ const ChatInput = () => {
   return (
     <div className="w-full flex px-3 text-base">
       <textarea className="flex-1 rounded-full bg-[#f4f4f4] max-h-[25dvh]" />
-      <button className="rounded-full bg-[#d7d7d7] p-3 m-2">Send</button>
+      <button className="rounded-full bg-[#d7d7d7] py-3 px-5 mx-2">
+        &#9654;
+      </button>
     </div>
   )
 }
